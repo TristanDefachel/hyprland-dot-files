a7d7ff0c0be507b3ed7974dc0d199da02e6a16c7
Trist4n <tristan@defachel.fr>
2026-06-30 22:41:34 +0200

    fix(waybar): Notification blink

--- waybar/scripts/swaync-watcher.sh
+++ waybar/scripts/swaync-watcher.sh
@@ -3,7 +3,13 @@
 # --- Swaync Watcher (No dependencies) ---
 
 swaync-client -swb | while IFS= read -r line; do
-    echo "$line" | \
+    line=$(echo "$line" | \
     sed -E 's/"text"\s*:\s*0/"text":""/g; s/"text"\s*:\s*"0"/"text":""/g' | \
-    sed -E 's/"text"\s*:\s*"([1-9][0-9]*)"/"text":" \1"/g; s/"text"\s*:\s*([1-9][0-9]*)/"text":" \1"/g'
+    sed -E 's/"text"\s*:\s*"([1-9][0-9]*)"/"text":" \1"/g; s/"text"\s*:\s*([1-9][0-9]*)/"text":" \1"/g')
+
+    if echo "$line" | grep -q '"text":" [0-9]'; then
+        line=$(echo "$line" | sed 's/}/,"class":["new-notification"]}/')
+    fi
+
+    echo "$line"
 done
